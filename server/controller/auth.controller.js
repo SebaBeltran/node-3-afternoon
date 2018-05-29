46b7fef9e8197925983c1a04661e83d1227b09b1
Seba Beltran <seba@trip.com.uy>
2018-05-29 15:54:51 -0600

    controllers completed

--- server/controller/auth.controller.js
+++ server/controller/auth.controller.js
@@ -3,7 +3,17 @@ const users = require("../models/users")
 let id = 1;
 
 module.exports = {
-  login:(req, res, next)=>{},
+  login:(req, res, next)=>{
+    let user = users.find(obj => obj.username === req.session.user.username && obj.password === req.session.user.password);
+    user !== undefined
+      ?(
+      req.session.user.username = user.username,
+      res.status(200).send(req.session.user)
+      )
+      :
+      res.status(500).send("no")
+  },
+
   register:(req, res, next)=>{
     users.push({
       id: id,
@@ -11,9 +21,16 @@ module.exports = {
       password: req.body.password
     })
     id++
-    res.status(200).send(req.session.users)
-    next()
+    req.session.user.username = req.body.username
+    res.status(200).send(req.session.user)
   },
-  signout:(req, res, next)=>{},
-  getUser:(req, res, next)=>{}
+
+  signout:(req, res, next)=>{
+    req.session.destroy()
+    res.status(200).send(req.session)
+  },
+
+  getUser:(req, res, next)=>{
+    res.status(200).send(req.session.user)
+  }
 }
